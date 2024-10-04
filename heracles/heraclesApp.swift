8f39a5c33b214feda5bdd18aa37103400ba61e25
uped <git@uped.dev>
2024-10-04 12:25:37 +0200

    Initial Commit

---
+++ heracles/heraclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  heraclesApp.swift
+//  heracles
+//
+//  Created by Miłosz Koczorowski on 04/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct heraclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ heraclesTests/heraclesTests.swift
@@ -0,0 +1,17 @@
+//
+//  heraclesTests.swift
+//  heraclesTests
+//
+//  Created by Miłosz Koczorowski on 04/10/2024.
+//
+
+import Testing
+@testable import heracles
+
+struct heraclesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
