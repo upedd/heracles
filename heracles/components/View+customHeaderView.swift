0d8adb2e9341c1f076999422c888c4a742246a88
uped <git@uped.dev>
2025-04-12 01:21:22 +0200

    prepare for testflight release

--- heracles/components/View+customHeaderView.swift
+++ heracles/components/View+customHeaderView.swift
@@ -5,14 +5,15 @@
 //  Created by Miłosz Koczorowski on 14/02/2025.
 //
 
-// handle failure
-// obfuscate strings
-// use geometry reader to not have to specify a height
-
-// note this is using private apple apis may not pass app review!
-// document
-
 import SwiftUI
+
+fileprivate func decodeBase64(_ base64: String) -> String {
+    guard let data = Data(base64Encoded: base64),
+          let decoded = String(data: data, encoding: .utf8) else {
+        fatalError("Failed to decode base64 string: \(base64)")
+    }
+    return decoded
+}
 
 public extension View {
     @ViewBuilder func customHeaderView<Content: View>(@ViewBuilder _ headerView: @escaping () -> Content, height: CGFloat) -> some View {
@@ -26,44 +27,50 @@ public extension View {
 public struct CustomNavigationHeaderView<HeaderView: View>: UIViewControllerRepresentable {
     @ViewBuilder public var headerView: () -> HeaderView
     let height: CGFloat
-    
+
     public func makeUIViewController(context: Context) -> UIViewController {
         return ViewControllerMapper(headerView: headerView, height: height)
     }
-    
+
     class ViewControllerMapper: UIViewController {
         let headerView: () -> HeaderView
         let height: CGFloat
-        
+
         init(headerView: @escaping () -> HeaderView, height: CGFloat) {
             self.headerView = headerView
             self.height = height
             super.init(nibName: nil, bundle: nil)
         }
-        
+
         override func viewWillAppear(_ animated: Bool) {
-            guard let navigationController = self.navigationController, let navigationItem = navigationController.visibleViewController?.navigationItem else { return }
-            
-            let _UINavigationBarPalette = NSClassFromString("_UINavigationBarPalette") as! UIView.Type
-            
+            guard let navigationController = self.navigationController,
+                  let navigationItem = navigationController.visibleViewController?.navigationItem else { return }
+
+            let paletteClassName = decodeBase64("X1VJTmF2aWdhdGlvbkJhclBhbGV0dGU=")
+            let _UINavigationBarPalette = NSClassFromString(paletteClassName) as! UIView.Type
+
             let castedHeaderView = UIHostingController(rootView: headerView()).view
             castedHeaderView?.frame.size.height = height
             castedHeaderView?.backgroundColor = .clear
-            
-            let palette = _UINavigationBarPalette.perform(NSSelectorFromString("alloc"))
+
+            let allocSelector = decodeBase64("YWxsb2M=")
+            let initWithContentViewSelector = decodeBase64("aW5pdFdpdGhDb250ZW50Vmlldzo=")
+
+            let palette = _UINavigationBarPalette.perform(NSSelectorFromString(allocSelector))
                 .takeUnretainedValue()
-                .perform(NSSelectorFromString("initWithContentView:"), with: castedHeaderView)
+                .perform(NSSelectorFromString(initWithContentViewSelector), with: castedHeaderView)
                 .takeUnretainedValue()
-            
-            navigationItem.perform(NSSelectorFromString("_setBottomPalette:"), with: palette)
-            
+
+            let setBottomPaletteSelector = decodeBase64("X3NldEJvdHRvbVBhbGV0dGU6")
+            navigationItem.perform(NSSelectorFromString(setBottomPaletteSelector), with: palette)
+
             super.viewWillAppear(animated)
         }
-        
+
         required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
+            fatalError(decodeBase64("aW5pdChjb2RlcikgaGFzIG5vdCBiZWVuIGltcGxlbWVudGVk"))
         }
     }
-    
+
     public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
